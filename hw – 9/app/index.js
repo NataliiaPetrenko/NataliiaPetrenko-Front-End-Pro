8c862daec32b8d4e364f7beca23fbac4158f922a
Nataliia Petrenko <58181059+NataliiaPetrenko@users.noreply.github.com>
2024-03-27 23:46:42 +0200

    Update index.js

--- hw – 9/app/index.js
+++ hw – 9/app/index.js
@@ -1,6 +1,6 @@
 const EXCHANGE_RATE = 26;
 
 for (let dollarAmount = 10; dollarAmount <= 100; dollarAmount += 10) {
-    const uahAmount = dollarAmount * exchangeRate;
+    const uahAmount = dollarAmount * EXCHANGE_RATE;
     console.log(`${dollarAmount} is ${uahAmount} UAH.`);
 }
