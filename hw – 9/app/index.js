b10d2005f92110c5b03d73f9a8c745523e54da89
Nataliia Petrenko <58181059+NataliiaPetrenko@users.noreply.github.com>
2024-03-27 23:43:53 +0200

    Add files via upload

---
+++ hw – 9/app/index.js
@@ -0,0 +1,6 @@
+const exchangeRate = 26;
+
+for (let dollarAmount = 10; dollarAmount <= 100; dollarAmount += 10) {
+    const uahAmount = dollarAmount * exchangeRate;
+    console.log(`${dollarAmount} is ${uahAmount} UAH.`);
+}
